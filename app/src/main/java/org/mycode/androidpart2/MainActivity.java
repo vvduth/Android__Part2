02a3c74f7603acaa52c9caa6d55468d4a6d348a9
vvduth <ducthai060501@gmail.com>
2021-07-20 22:54:16 +0300

    "Snackbar"

--- app/src/main/java/org/mycode/androidpart2/MainActivity.java
+++ app/src/main/java/org/mycode/androidpart2/MainActivity.java
@@ -1,14 +1,49 @@
 package org.mycode.androidpart2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout parent;
+    private Button btnShowSnackBar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parent = findViewById(R.id.parent);
+        btnShowSnackBar = findViewById(R.id.btnShowSnackBar);
+
+        btnShowSnackBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSnackBar();
+            }
+        });
+    }
+
+    private void showSnackBar(){
+        Snackbar.make(parent, "This is a snack bar", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Retry Clicked", Toast.LENGTH_LONG).show();
+                    }
+                })
+
+        .setActionTextColor(getResources().getColor(R.color.purple_200)) //change color of the actionalbe things :v
+        //.setActionTextColor(Color.RED); can also use this to change color of the snack bar
+                .setTextColor(Color.YELLOW) //change color of the text in the bar
+        .show();
+
     }
 }
